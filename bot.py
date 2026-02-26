e2cc777bde848df06973abce2b5c355c59fca6c8
qateshusan-lab <qatesthusan@gmail.com>
2026-02-27 04:04:05 +0500

    clean bot file

--- bot.py
+++ bot.py
@@ -1,43 +1,6 @@
-
-import os
-import asyncio
-from telegram import Update
-from telegram.ext import (
-    ApplicationBuilder,
-    CommandHandler,
-    MessageHandler,
-    ContextTypes,
-    filters,
-)
-
-BOT_TOKEN = os.getenv("BOT_TOKEN")
-
-
-async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    print("START BOSILDI")
-    await update.message.reply_text("Bot ishlayapti!")
-
-
-async def echo(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    print("XABAR:", update.message.text)
-    await update.message.reply_text(f"Siz yozdingiz: {update.message.text}")
-
-
-async def main():
-    app = ApplicationBuilder().token(BOT_TOKEN).build()
-
-    app.add_handler(CommandHandler("start", start))
-    app.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, echo))
-
-    print("Bot ishga tushdi...")
-    await app.run_polling()
-
-
-if __name__ == "__main__":
-    asyncio.run(main())
-=======
 import logging
 import random
+import os
 from telegram import Update, ReplyKeyboardMarkup
 from telegram.ext import (
     Application,
@@ -47,10 +10,11 @@ from telegram.ext import (
     filters,
 )
 
-TOKEN = "8421948716:AAFABgCiFG-z07SQ7rIDRmK5p6Ey9i3WV4o"
+TOKEN = os.getenv("BOT_TOKEN")
 
 logging.basicConfig(
-    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s", level=logging.INFO
+    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
+    level=logging.INFO,
 )
 
 grammar_lessons = {
@@ -126,4 +90,3 @@ def main():
 
 if __name__ == "__main__":
     main()
-
