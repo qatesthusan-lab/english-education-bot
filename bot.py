c75946c6be2e45cd2053ccc7018ab8d161e4a3d2
qateshusan-lab <qatesthusan@gmail.com>
2026-02-27 03:14:54 +0500

    bot async fixed

--- bot.py
+++ bot.py
@@ -1,14 +1,15 @@
 import os
+import asyncio
 from telegram import Update
 from telegram.ext import (
-    Application,
+    ApplicationBuilder,
     CommandHandler,
     MessageHandler,
     ContextTypes,
     filters,
 )
 
-BOT_TOKEN = os.environ["BOT_TOKEN"]
+BOT_TOKEN = os.getenv("BOT_TOKEN")
 
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
@@ -17,19 +18,19 @@ async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
 
 
 async def echo(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    print("XABAR KELDI:", update.message.text)
-    await update.message.reply_text("Siz yozdingiz: " + (update.message.text or ""))
+    print("XABAR:", update.message.text)
+    await update.message.reply_text(f"Siz yozdingiz: {update.message.text}")
 
 
-def main():
-    app = Application.builder().token(BOT_TOKEN).build()
+async def main():
+    app = ApplicationBuilder().token(BOT_TOKEN).build()
 
     app.add_handler(CommandHandler("start", start))
     app.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, echo))
 
-    print("Bot ishga tushdi")
-    app.run_polling()
+    print("Bot ishga tushdi...")
+    await app.run_polling()
 
 
 if __name__ == "__main__":
-    main()
+    asyncio.run(main())
